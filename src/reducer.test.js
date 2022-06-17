9b0a939a52c93705f2bedb24bd447fb6f9a0e0e3
Jason Aricheta <jason.aricheta@gmail.com>
2022-06-18 11:06:52 +1200

    completed exercises 6.1-6.2

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,7 +2,7 @@ import deepFreeze from 'deep-freeze'
 import counterReducer from './reducer'
 
 describe('unicafe reducer', () => {
-  const initialState = {
+  const initialState = { // in scope for all tests
     good: 0,
     ok: 0,
     bad: 0
@@ -24,10 +24,61 @@ describe('unicafe reducer', () => {
     }
     const state = initialState
 
-    deepFreeze(state)
+    deepFreeze(state) // ensures that the state is NOT mutated
     const newState = counterReducer(state, action)
     expect(newState).toEqual({
       good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state) // ensures that the state is NOT mutated
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })  
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state) // ensures that the state is NOT mutated
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })    
+
+  test('two goods increments twice', () => {
+    // ARRANGE
+    const action = {
+      type: 'GOOD'
+    }
+
+    const state = initialState
+
+    // ACT
+    deepFreeze(state) // minor ASSERT for frozen object
+    let newState = counterReducer(state, action)
+    newState = counterReducer(newState, action) // twice
+
+    // ASSERT
+    expect(newState).toEqual({
+      good: 2,
       ok: 0,
       bad: 0
     })
